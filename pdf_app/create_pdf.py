00b633679c6dad33f94b5d0e6f8a2131f0074c79
DN24596 <129110045+DN24596@users.noreply.github.com>
2023-04-07 18:16:29 -0500

    Update create_pdf.py

--- pdf_app/create_pdf.py
+++ pdf_app/create_pdf.py
@@ -1,66 +1,45 @@
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.enums import TA_CENTER
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
-from reportlab.lib.units import inch
+import io
+from datetime import date
+from PyPDF2 import PdfFileWriter, PdfFileReader
+from reportlab.pdfgen import canvas
 
-# Define data for the form fields
-first_name = "John"
-last_name = "Doe"
-dob = "01/01/1970"
-current_date = "03/23/2023"
-member_id = "123456"
-claim_number = "789012"
+def generate_pdf(first_name, last_name, dob, member_id, claim_num):
+    # Create a new PDF file
+    packet = io.BytesIO()
+    can = canvas.Canvas(packet)
 
-# Define a function to create a paragraph with a specified style and text
-def create_paragraph(text, style):
-    return Paragraph(text, style)
+    # Set the font size and add the fields
+    can.setFontSize(12)
+    can.drawString(100, 700, 'First Name: {}'.format(first_name))
+    can.drawString(100, 670, 'Last Name: {}'.format(last_name))
+    can.drawString(100, 640, 'Date of Birth: {}'.format(dob))
+    can.drawString(100, 610, 'Current Date: {}'.format(date.today()))
+    can.drawString(100, 580, 'Member ID: {}'.format(member_id))
+    can.drawString(100, 550, 'Claim Number: {}'.format(claim_num))
 
-# Define the styles for the form fields
-styles = getSampleStyleSheet()
-styles.add(ParagraphStyle(name='Center', alignment=TA_CENTER))
-styles.add(ParagraphStyle(name='Signature', alignment=TA_CENTER, fontSize=12))
+    # Add a signature field
+    can.line(100, 500, 250, 500)
+    can.drawString(100, 480, 'Signature')
 
-# Define the content for the PDF form
-content = []
+    # Save the PDF file
+    can.save()
 
-# Add a spacer to separate the fields
-content.append(Spacer(1, 0.25 * inch))
+    # Move to the beginning of the file and create a PdfFileReader object
+    packet.seek(0)
+    new_pdf = PdfFileReader(packet)
 
-# Add the first and last name fields
-content.append(create_paragraph("First Name:", styles['Normal']))
-content.append(create_paragraph(first_name, styles['Normal']))
-content.append(create_paragraph("Last Name:", styles['Normal']))
-content.append(create_paragraph(last_name, styles['Normal']))
+    # Load the existing PDF document
+    existing_pdf = PdfFileReader(open('path/to/existing/pdf', 'rb'))
 
-# Add a spacer to separate the fields
-content.append(Spacer(1, 0.25 * inch))
+    # Create a PdfFileWriter object for the output PDF
+    output = PdfFileWriter()
 
-# Add the date of birth and current date fields
-content.append(create_paragraph("Date of Birth:", styles['Normal']))
-content.append(create_paragraph(dob, styles['Normal']))
-content.append(create_paragraph("Current Date:", styles['Normal']))
-content.append(create_paragraph(current_date, styles['Normal']))
+    # Add the new page to the output PDF
+    page = existing_pdf.getPage(0)
+    page.mergePage(new_pdf.getPage(0))
+    output.addPage(page)
 
-# Add a spacer to separate the fields
-content.append(Spacer(1, 0.25 * inch))
-
-# Add the member ID and claim number fields
-content.append(create_paragraph("Member ID:", styles['Normal']))
-content.append(create_paragraph(member_id, styles['Normal']))
-content.append(create_paragraph("Claim Number:", styles['Normal']))
-content.append(create_paragraph(claim_number, styles['Normal']))
-
-# Add a spacer to separate the fields
-content.append(Spacer(1, 0.5 * inch))
-
-# Add a signature field
-content.append(create_paragraph("Signature:", styles['Normal']))
-content.append(create_paragraph("______________________________", styles['Signature']))
-
-# Define the filename for the generated PDF file
-filename = "insurance_claim.pdf"
-
-# Generate the PDF form
-doc = SimpleDocTemplate(filename, pagesize=letter)
-doc.build(content)
+    # Save the output PDF to a file
+    outputStream = open("output.pdf", "wb")
+    output.write(outputStream)
+    outputStream.close()
